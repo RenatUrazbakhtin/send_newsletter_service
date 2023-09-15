07afadf3a9b9b618217c3957e697dffadafb199b
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-15 17:09:39 +0300

    CRUD for client

--- main/views.py
+++ main/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from main.models import Client
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+
+class ClientListView(ListView):
+    model = Client
+
+class ClientDetailView(DetailView):
+    model = Client
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'first_name', 'last_name', 'comment']
+    success_url = reverse_lazy('client_list')
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'first_name', 'last_name', 'comment']
+    def get_success_url(self):
+        return reverse('client_list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('client_list')
+
+
+
