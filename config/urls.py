12304a116e68da40b46fba76f8d8ee8b7553fc10
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-17 13:45:42 +0300

    add url for app newsletter

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
