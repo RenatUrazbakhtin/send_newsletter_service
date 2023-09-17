404d6cbf62e5f13ce480ddf6230722b107fbeb39
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-17 13:51:27 +0300

    changes for correct work

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main.models import Client, NewsletterSettings, NewsletterMessage, NewsletterLog
+from main.models import Client
 
 
 # Register your models here.
@@ -10,20 +10,3 @@ class ClientAdmin(admin.ModelAdmin):
     list_filter = ('email', 'first_name', 'last_name',)
     search_fields = ('email', 'first_name', 'last_name',)
 
-@admin.register(NewsletterSettings)
-class NewsletterSettingsAdmin(admin.ModelAdmin):
-    list_display = ('newsletter_time_from', 'newsletter_time_to', 'periodicity', 'status')
-    list_filter = ('status',)
-    search_fields = ('periodicity', 'subject',)
-
-@admin.register(NewsletterMessage)
-class NewsletterMessageAdmin(admin.ModelAdmin):
-    list_display = ('title','body',)
-    list_filter = ('title',)
-    search_fields = ('title','body',)
-
-@admin.register(NewsletterLog)
-class NewsletterLogAdmin(admin.ModelAdmin):
-    list_display = ('last_attempt_time','feedback','attempt_status',)
-    list_filter = ('attempt_status',)
-    search_fields = ('last_attempt_time',)
