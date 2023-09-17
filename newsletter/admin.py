00ee03fae1acf0554f379403f4e093920fbf3182
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-17 13:51:59 +0300

    creating admin for newsletter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from newsletter.models import NewsletterSettings, NewsletterMessage, NewsletterLog
+
+
 # Register your models here.
+@admin.register(NewsletterSettings)
+class NewsletterSettingsAdmin(admin.ModelAdmin):
+    list_display = ('newsletter_time_from', 'newsletter_time_to', 'periodicity', 'status')
+    list_filter = ('status',)
+    search_fields = ('periodicity', 'subject',)
+
+@admin.register(NewsletterMessage)
+class NewsletterMessageAdmin(admin.ModelAdmin):
+    list_display = ('title','body',)
+    list_filter = ('title',)
+    search_fields = ('title','body',)
+
+@admin.register(NewsletterLog)
+class NewsletterLogAdmin(admin.ModelAdmin):
+    list_display = ('last_attempt_time','feedback','attempt_status',)
+    list_filter = ('attempt_status',)
+    search_fields = ('last_attempt_time',)
