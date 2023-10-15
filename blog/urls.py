b98be40906fe340653bdb0c4450d3f7f207d726c
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-15 04:05:39 +0300

    cache correct

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogDeleteV
 urlpatterns = [
     path('blogs_list/', cache_page(60)(BlogListView.as_view()), name='blogs_list'),
     path('Create_blog/', cache_page(60)(BlogCreateView.as_view()), name='create_blog'),
-    path('blog_detail/<int:pk>/', cache_page(60)(BlogDetailView).as_view(), name='blog_detail'),
+    path('blog_detail/<int:pk>/', cache_page(60)((BlogDetailView).as_view()), name='blog_detail'),
     path('blog_delete/<int:pk>/', cache_page(60)(BlogDeleteView.as_view()), name='blog_delete'),
     path('blog_update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update')
 ]
