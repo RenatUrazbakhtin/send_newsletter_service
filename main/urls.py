ca6442cfce8715d6b63b93c81288d78ff8914b88
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-15 03:23:11 +0300

    small changes with home page

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from main.views import ClientCreateView, ClientUpdateView, ClientDeleteView, ClientListView, home
+from main.views import ClientCreateView, ClientUpdateView, ClientDeleteView, ClientListView, HomeView
 
 urlpatterns = [
     path('create_client/', ClientCreateView.as_view(), name='create_client'),
     path('update_client/<int:pk>/', ClientUpdateView.as_view(), name='update_client'),
     path('delete_client/<int:pk>/', ClientDeleteView.as_view(), name='delete_client'),
     path('client_list/', ClientListView.as_view(), name='client_list'),
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
 ]
