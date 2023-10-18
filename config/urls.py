946ced395500f03b9d9d97ca0c7b792260366afa
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-18 23:40:17 +0300

    add users url

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include('newsletter.urls')),
     path('', include('blog.urls')),
+    path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
