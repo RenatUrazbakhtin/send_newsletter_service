af3a902c8df17bf779682395f41b23e63aba773b
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-18 23:40:57 +0300

    changes for auth user

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from config import settings
+
 # Create your models here.
 NULLABLE = {'null': True, 'blank': True}
 class Client(models.Model):
@@ -7,7 +9,7 @@ class Client(models.Model):
     first_name = models.CharField(max_length=100, verbose_name='Имя', **NULLABLE)
     last_name = models.CharField(max_length=100, verbose_name='Фамилия', **NULLABLE)
     comment = models.TextField(max_length=1000, verbose_name='Комментарий')
-
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name = 'создатель рассылки', ** NULLABLE)
     def __srt__(self):
         return f'{self.first_name} {self.last_name}'
 
